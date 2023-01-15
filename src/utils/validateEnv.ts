a65a0bf279a8e39d0e8b71a319e5b88d740e5c19
Julius Uusinarkaus <julius@uusinarkaus.com>
2023-01-15 13:28:41 +0200

    deploy: add fly

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -4,11 +4,12 @@ const validateEnv = () => {
   cleanEnv(process.env, {
     NODE_ENV: str(),
     PORT: port(),
-    POSTGRES_HOST: str(),
-    POSTGRES_PORT: port(),
-    POSTGRES_USER: str(),
-    POSTGRES_PASSWORD: str(),
-    POSTGRES_DB: str(),
+    // POSTGRES_HOST: str(),
+    // POSTGRES_PORT: port(),
+    // POSTGRES_USER: str(),
+    // POSTGRES_PASSWORD: str(),
+    // POSTGRES_DB: str(),
+    DATABASE_URL: str(),
   });
 };
 
